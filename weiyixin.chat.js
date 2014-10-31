d397ca5bd250cac5069c9f8c5a6ef19b5aee01f2
wang_zhen <wang_zhen@corp.netease.com>
2014-10-31 16:06:45 +0800

    add index

--- weiyixin.chat.js
+++ weiyixin.chat.js
@@ -1,24 +1,13 @@
-var shareData = {
-	'link' = 'http://jiankang.163.com/special/weixin_quiz/',
-	'imgUrl' = 'http://img1.cache.netease.com/f2e/health/weixin_quiz/images/120x120-5star.jpg',
-	'imgSize' = '300',
-	'title' = '测测你的“白领症”中招指数吧！',
-	'desc' = '我刚测完自己的”白领症“中招指数，你也来测测你的“白领症”中招指数吧！',
-	'appid' = '',
-	'callback' = function(){
-		console.log("I'm back!")
-	}
-}
-
 var nyphile = function() {};
 var _callback = shareData.callback || nyphile;
 var _size = shareData.imgSize || 200;
 var _title = shareData.title || document.title;
 var _link = shareData.link || window.location.href;
 var _desc = shareData.desc || document.title;
+var _appid = shareData.appid || '';
 function shareFriend(){
 	WeixinJSBridge.invoke('sendAppMessage',{
-		"appid": shareData.appid,
+		"appid": _appid,
 		"img_url": shareData.imgUrl,
 		"img_width": _size,
 		"img_height": _size,
@@ -29,7 +18,7 @@ function shareFriend(){
 }
 function shareTimeline() {
 	WeixinJSBridge.invoke('shareTimeline',{
-		"appid": shareData.appid,
+		"appid": _appid,
 		"img_url": shareData.imgUrl,
 		"img_width": _size,
 		"img_height": _size,
@@ -46,7 +35,7 @@ function shareWeibo() {
 }
 function shareFriendYi() {
 	YixinJSBridge.invoke('sendAppMessage',{
-		"appid": shareData.appid,
+		"appid": _appid,
 		"img_url": shareData.imgUrl,
 		"img_width": _size,
 		"img_height": _size,
@@ -57,7 +46,7 @@ function shareFriendYi() {
 }
 function shareTimelineYi() {
 	YixinJSBridge.invoke('shareTimeline',{
-		"appid": shareData.appid,
+		"appid": _appid,
 		"img_url": shareData.imgUrl,
 		"img_width": _size,
 		"img_height": _size,
